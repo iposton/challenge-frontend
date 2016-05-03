5a36d791cb387b6c2fe17444afd88eb1c540036f
iposton <ianposton@sbcglobal.net>
2016-05-03 11:23:50 -0700

    filter data in table view when multiple items checked show hide table divs when checked format date and percents

--- src/app/components/example-directive/example-directive.directive.js
+++ src/app/components/example-directive/example-directive.directive.js
@@ -36,21 +36,75 @@
             var selectedBrowsers = {};
             var hasSelectedBrowsers = false;
 
-          scope.toggle = function(b) {
-            if (selectedBrowsers[b.browser] == null) {
-                selectedBrowsers[b.browser] = true;
-                if(Object.keys(selectedBrowsers).length >= 1){scope.hasSelectedBrowsers = true;}
-                else{scope.hasSelectedBrowsers = false;}
+            // create object when browser is checked in view
+            scope.toggle = function(b) {
+                if (selectedBrowsers[b.browser] == null) {
+                    selectedBrowsers[b.browser] = true;
+
+                    if (Object.keys(selectedBrowsers).length >= 1) { scope.hasSelectedBrowsers = true; } else { scope.hasSelectedBrowsers = false; }
+                } else {
+                    delete selectedBrowsers[b.browser];
+                    if (Object.keys(selectedBrowsers).length >= 1) { scope.hasSelectedBrowsers = true; } else { scope.hasSelectedBrowsers = false; }
+                }
+
+                vm.selectedBrowsers = selectedBrowsers;
+                console.log(Object.keys(selectedBrowsers));
+                vm.hasSelectedBrowsers = hasSelectedBrowsers;
+            };
+
+            // Create a simple toggle when clicks is checked use fat arrow ES2015 syntax in place of the function key word
+            scope.showClicks = () => {
+                vm.clicks = !vm.clicks;
             }
-            else {
-                delete selectedBrowsers[b.browser];
-                if(Object.keys(selectedBrowsers).length >= 1){scope.hasSelectedBrowsers = true;}
-                else{scope.hasSelectedBrowsers = false;}
+
+            scope.showCompleteImpressions = () => {
+                vm.completions = !vm.completions;
             }
-           vm.selectedBrowsers = selectedBrowsers;
-           console.log(Object.keys(selectedBrowsers));
-           vm.hasSelectedBrowsers = hasSelectedBrowsers;
-        };
+
+            scope.showFirstqPercent = () => {
+                vm.firstqPercent = !vm.firstqPercent;
+            }
+
+            scope.showMidqPercent = () => {
+                vm.midpointPercent = !vm.midpointPercent;
+            }
+
+            scope.showThirdqPercent = () => {
+                vm.thirdPercent = !vm.thirdPercent;
+            }
+
+            scope.showFirstQ = () => {
+                vm.firstqImpressions = !vm.firstqImpressions;
+            }
+
+            scope.showMid = () => {
+                vm.midpointImpressions = !vm.midpointImpressions;
+            }
+
+            scope.showThirdQ = () => {
+                vm.thirdqImpressions = !vm.thirdqImpressions;
+            }
+
+
+            // format pecentages for view
+            scope.displayPercent = function(percent) {
+                return (percent * 100).toFixed(3);
+            };
+
+            // format pecentages for view
+            scope.displayCompletePercent = function(percent) {
+                return (percent * 100).toFixed(0);
+            };
+
+            // Filter Browser Data in table view
+            scope.searchBy = function() {
+                return function(b) {
+
+                    if (selectedBrowsers[b.browser] === true || Object.keys(selectedBrowsers).length == 0) {
+                        return true;
+                    }
+                }
+            };
 
             var dataWatcher = scope.$watch("vm.data", doSomething);
 
@@ -81,7 +135,6 @@
 
         var vm = this;
 
-
         vm.browsers = MockDataBrowsers;
         //console.log(vm.browsers);
 
@@ -90,57 +143,58 @@
         var mb = [];
         var browserMetrics = [];
 
-         for (var i = 0; i < vm.browsers.length; i++) {
+        // Parse data and push items into an array 
+        for (var i = 0; i < vm.browsers.length; i++) {
             var browser = vm.browsers[i];
-                browserNames.push(browser);
-                browserNames.splice(4);
-                browserNamesMobile.push(browser);
-                var mb = browserNamesMobile.slice(4, 7);
-                browserMetrics.push(browser);
-                browserMetrics.splice(1);
+            browserNames.push(browser);
+            browserNames.splice(4);
+            browserNamesMobile.push(browser);
+            var mb = browserNamesMobile.slice(4, 7);
+            browserMetrics.push(browser);
+            //browserMetrics.splice(1);
 
-                 if(browser.browser == "chrome") {
-                    browser.label = "Chrome";
-                    browser.platform = "Desktop";
-                  }
+            if (browser.browser == "chrome") {
+                browser.label = "Chrome";
+                browser.platform = "Desktop";
+            }
 
-                 if(browser.browser == "ie") {
-                    browser.label = "Internet Explorer";
-                    browser.platform = "Desktop";
-                 }
+            if (browser.browser == "ie") {
+                browser.label = "Internet Explorer";
+                browser.platform = "Desktop";
+            }
 
-                 if(browser.browser == "safari") {
-                    browser.label = "Safari";
-                    browser.platform = "Desktop";
-                 }
-                
-                 if(browser.browser == "firefox") {
-                    browser.label = "Firefox";
-                    browser.platform = "Desktop";
-                 }
+            if (browser.browser == "safari") {
+                browser.label = "Safari";
+                browser.platform = "Desktop";
+            }
 
-                if(browser.browser == "chrome-mobile") {
-                    browser.label = "Chrome";
-                    browser.platform = "Desktop";
-                 }
+            if (browser.browser == "firefox") {
+                browser.label = "Firefox";
+                browser.platform = "Desktop";
+            }
 
-                 if(browser.browser == "androidwebkit") {
-                    browser.label = "Android Webkit";
-                    browser.platform = "Desktop";
-                 }
-                
-                 if(browser.browser == "mobile-safari") {
-                    browser.label = "Safari";
-                    browser.platform = "Desktop";
-                 }
-            
-          }
-           vm.browser = browser;
+            if (browser.browser == "chrome-mobile") {
+                browser.label = "Chrome";
+                browser.platform = "Mobile";
+            }
 
-           vm.browserNames = browserNames;
-           vm.mobileBrowsers = mb;
-           vm.browserMetrics = browserMetrics;
-           console.log(vm.browser);
+            if (browser.browser == "androidwebkit") {
+                browser.label = "Android Webkit";
+                browser.platform = "Mobile";
+            }
+
+            if (browser.browser == "mobile-safari") {
+                browser.label = "Safari";
+                browser.platform = "Mobile";
+            }
+
+        }
+
+        vm.browser = browser;
+        vm.browserNames = browserNames;
+        vm.mobileBrowsers = mb;
+        vm.browserMetrics = browserMetrics;
+        //console.log(vm.browsers);
 
     }
 
